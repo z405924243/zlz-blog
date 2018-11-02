66a6983630ab4ae7059ea33fb9ebc1cde73ecffd
zlz <405924243@qq.com>
2018-11-02 16:33:55 +0800

    1

--- blog_back_management/src/components/main.js
+++ blog_back_management/src/components/main.js
@@ -1,7 +1,7 @@
-import { Layout, Menu, Breadcrumb, Icon } from "antd";
+import { Layout, Menu, Icon, Card } from "antd";
 import React from "react";
 
-const { Header, Content, Footer, Sider } = Layout;
+const { Header, Content, Sider } = Layout;
 
 export default class SiderMenu extends React.Component {
   state = {
@@ -48,18 +48,16 @@ export default class SiderMenu extends React.Component {
             </Menu>
           </Sider>
           <Layout>
-            <Content style={{ margin: "0 16px" }}>
-              <Breadcrumb style={{ margin: "16px 0" }}>
+            <Content style={{ margin: "16px" }}>
+              {/* <Breadcrumb style={{ margin: "16px 0" }}>
                 <Breadcrumb.Item>User</Breadcrumb.Item>
-                <Breadcrumb.Item>Bill</Breadcrumb.Item>
-              </Breadcrumb>
-              <div style={{ padding: 24, background: "#fff", minHeight: 360 }}>
-                Bill is a cat.
-              </div>
+              </Breadcrumb> */}
+              <Card bordered={false}>
+                <p>Card content</p>
+                <p>Card content</p>
+                <p>Card content</p>
+              </Card>
             </Content>
-            <Footer style={{ textAlign: "center" }}>
-              Ant Design ©2018 Created by Ant UED
-            </Footer>
           </Layout>
         </Layout>
       </Layout>
---
+++ blog_back_management/src/components/writeBlog.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+
+export default class WriteBlog extends React.Component {
+    constructor(){}
+    state = {}
+}
